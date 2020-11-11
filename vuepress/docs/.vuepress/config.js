6544ced52ee73b9d88ca3eb7b50f13d58cbc2120
flyeagleyuan <554010635@qq.com>
2020-11-11 17:08:47 +0800

    feat: 虚拟DOM

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -1,26 +1,31 @@
 module.exports = {
-  title: "记录vue源码，让你更懂vue",
-  description: "使用了这么长时间得vue，特别想自己写一份源码得分析，便于自己理解，也输出一些东西",
+  title: '记录vue源码，让你更懂vue',
+  description: '使用了这么长时间得vue，特别想自己写一份源码得分析，便于自己理解，也输出一些东西',
   themeConfig: {
     displayAllHeaders: true,
     sidebarDepth: 2,
     sidebar: [
-      {title:'前言',path:'/',children:['/']},
+      { title: '前言', path: '/', children: ['/'] },
       {
-        title:'数据变化',
-        path:'/changeObserve',
+        title: '数据变化',
+        path: '/changeObserve',
         // sidebarDepth: 2,
-        children:['changeObserve/','changeObserve/objectObserve','changeObserve/arrayObserve']
+        children: ['changeObserve/', 'changeObserve/objectObserve', 'changeObserve/arrayObserve'],
       },
-      {title:'前言',path:'/',children:['/']},
-      {title:'前言',path:'/',children:['/']}
+      {
+        title: '虚拟DOM',
+        path: '/virtualDom',
+        children: ['virtualDom/', 'virtualDom/DOMDiff', 'virtualDom/updateSubNode', 'virtualDom/improve'],
+      },
+      { title: '模板编译', path: '/template', children: ['template/'] },
+      { title: '生命周期', path: '/liftCycle', children: ['liftCycle/'] },
+      { title: '实例方法', path: '/instanceMethod', children: ['instanceMethod/'] },
+      { title: '全局API', path: '/globalAPI', children: ['globalAPI/'] },
     ],
-    lastUpdated: "Last Updated",
+    lastUpdated: 'Last Updated',
   },
-  markdown:{
-    lineNumbers: true
+  markdown: {
+    lineNumbers: true,
   },
-  plugins: [
-    'vuepress-plugin-mermaidjs'
-  ]
+  plugins: ['vuepress-plugin-mermaidjs'],
 };
